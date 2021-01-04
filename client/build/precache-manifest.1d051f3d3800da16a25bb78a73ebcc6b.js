9cebedc2e5e9c52faf8966b3a8c019bfc4b8e3ce
Karan Pandit <kp@Karans-MacBook-Pro.local>
2021-01-04 12:06:40 +1100

    make it better

---
+++ client/build/precache-manifest.1d051f3d3800da16a25bb78a73ebcc6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fcaef2e4cb63ca3c631f3a35493e6ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbf2e873708878f03523",
+    "url": "/static/css/main.6ebd63e0.chunk.css"
+  },
+  {
+    "revision": "ac7a5c3e32cd02e84a22",
+    "url": "/static/js/2.c166ff5e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c166ff5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbf2e873708878f03523",
+    "url": "/static/js/main.48b980f2.chunk.js"
+  },
+  {
+    "revision": "00fc4afc96365658fc09",
+    "url": "/static/js/runtime-main.eda899bb.js"
+  }
+]);
